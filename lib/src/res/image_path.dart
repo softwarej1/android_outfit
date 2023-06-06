28f58a00d4ce828cf4d4cc8456511521d894e979
kangsangjun <sangjunk>
2023-06-06 22:15:44 +0900

    06_06

--- lib/src/res/image_path.dart
+++ lib/src/res/image_path.dart
@@ -42,6 +42,10 @@ class ImagePath {
           'assets/images/man/spring/attendance/attendance2.jpg',
           'assets/images/man/spring/attendance/attendance3.jpg',
           'assets/images/man/spring/attendance/attendance4.jpg',
+          'assets/images/man/spring/attendance/attendance5.jpg',
+          'assets/images/man/spring/attendance/attendance6.jpg',
+          'assets/images/man/spring/attendance/attendance7.jpg',
+          'assets/images/man/spring/attendance/attendance8.jpg',
         ],
         // 캠퍼스
         [
@@ -50,6 +54,11 @@ class ImagePath {
           'assets/images/man/spring/campus/campus2.jpg',
           'assets/images/man/spring/campus/campus3.jpg',
           'assets/images/man/spring/campus/campus4.jpg',
+          'assets/images/man/spring/campus/campus5.jpg',
+          'assets/images/man/spring/campus/campus6.jpg',
+          'assets/images/man/spring/campus/campus7.jpg',
+          'assets/images/man/spring/campus/campus8.jpg',
+          'assets/images/man/spring/campus/campus9.jpg',
         ],
         // 데이트
         [
@@ -57,6 +66,11 @@ class ImagePath {
           'assets/images/man/spring/date/date1.jpg',
           'assets/images/man/spring/date/date2.jpg',
           'assets/images/man/spring/date/date3.jpg',
+          'assets/images/man/spring/date/date4.jpg',
+          'assets/images/man/spring/date/date5.jpg',
+          'assets/images/man/spring/date/date6.jpg',
+          'assets/images/man/spring/date/date7.jpg',
+          'assets/images/man/spring/date/date8.jpg',
         ],
         // 운동
         [
@@ -64,6 +78,11 @@ class ImagePath {
           'assets/images/man/spring/exercise/exercise1.jpg',
           'assets/images/man/spring/exercise/exercise2.jpg',
           'assets/images/man/spring/exercise/exercise3.jpg',
+          'assets/images/man/spring/exercise/exercise4.jpg',
+          'assets/images/man/spring/exercise/exercise5.jpg',
+          'assets/images/man/spring/exercise/exercise6.jpg',
+          'assets/images/man/spring/exercise/exercise7.jpg',
+          'assets/images/man/spring/exercise/exercise8.jpg',
         ],
       ],
       // 여름
@@ -75,34 +94,62 @@ class ImagePath {
           'assets/images/man/summer/attendance/attendance2.jpg',
           'assets/images/man/summer/attendance/attendance3.jpg',
           'assets/images/man/summer/attendance/attendance4.jpg',
+          'assets/images/man/summer/attendance/attendance5.jpg',
+          'assets/images/man/summer/attendance/attendance6.jpg',
+          'assets/images/man/summer/attendance/attendance7.jpg',
+          'assets/images/man/summer/attendance/attendance8.jpg',
+          'assets/images/man/summer/attendance/attendance9.jpg',
         ],
         // 캠퍼스
         [
           'assets/images/man/summer/campus/campus.jpg',
           'assets/images/man/summer/campus/campus1.jpg',
           'assets/images/man/summer/campus/campus2.jpg',
+          'assets/images/man/summer/campus/campus3.jpg',
+          'assets/images/man/summer/campus/campus4.jpg',
+          'assets/images/man/summer/campus/campus5.jpg',
+          'assets/images/man/summer/campus/campus6.jpg',
+          'assets/images/man/summer/campus/campus7.jpg',
+          'assets/images/man/summer/campus/campus8.jpg',
         ],
         // 데이트
         [
           'assets/images/man/summer/date/date.jpg',
           'assets/images/man/summer/date/date1.jpg',
           'assets/images/man/summer/date/date2.jpg',
+          'assets/images/man/summer/date/date3.jpg',
+          'assets/images/man/summer/date/date4.jpg',
+          'assets/images/man/summer/date/date5.jpg',
+          'assets/images/man/summer/date/date6.jpg',
+          'assets/images/man/summer/date/date7.jpg',
+          'assets/images/man/summer/date/date8.jpg',
         ],
         // 운동
         [
           'assets/images/man/summer/exercise/exercise.jpg',
           'assets/images/man/summer/exercise/exercise1.jpg',
           'assets/images/man/summer/exercise/exercise2.jpg',
+          'assets/images/man/summer/exercise/exercise3.jpg',
+          'assets/images/man/summer/exercise/exercise4.jpg',
+          'assets/images/man/summer/exercise/exercise5.jpg',
+          'assets/images/man/summer/exercise/exercise6.jpg',
+          'assets/images/man/summer/exercise/exercise7.jpg',
+          'assets/images/man/summer/exercise/exercise8.jpg',
         ],
       ],
       // 가을
       [
         // 출근
         [
+          'assets/images/man/fall/attendance/attendance.jpg',
           'assets/images/man/fall/attendance/attendance1.jpg',
           'assets/images/man/fall/attendance/attendance2.jpg',
           'assets/images/man/fall/attendance/attendance3.jpg',
-          'assets/images/man/fall/attendance/attendance.jpg',
+          'assets/images/man/fall/attendance/attendance4.jpg',
+          'assets/images/man/fall/attendance/attendance5.jpg',
+          'assets/images/man/fall/attendance/attendance6.jpg',
+          'assets/images/man/fall/attendance/attendance7.jpg',
+          'assets/images/man/fall/attendance/attendance8.jpg',
         ],
         // 캠퍼스
         [
@@ -110,6 +157,11 @@ class ImagePath {
           'assets/images/man/fall/campus/campus1.jpg',
           'assets/images/man/fall/campus/campus2.jpg',
           'assets/images/man/fall/campus/campus3.jpg',
+          'assets/images/man/fall/campus/campus4.jpg',
+          'assets/images/man/fall/campus/campus5.jpg',
+          'assets/images/man/fall/campus/campus6.jpg',
+          'assets/images/man/fall/campus/campus7.jpg',
+          'assets/images/man/fall/campus/campus8.jpg',
         ],
         // 데이트
         [
@@ -117,6 +169,11 @@ class ImagePath {
           'assets/images/man/fall/date/date1.jpg',
           'assets/images/man/fall/date/date2.jpg',
           'assets/images/man/fall/date/date3.jpg',
+          'assets/images/man/fall/date/date4.jpg',
+          'assets/images/man/fall/date/date5.jpg',
+          'assets/images/man/fall/date/date6.jpg',
+          'assets/images/man/fall/date/date7.jpg',
+          'assets/images/man/fall/date/date8.jpg',
         ],
         // 운동
         [
@@ -124,6 +181,11 @@ class ImagePath {
           'assets/images/man/fall/exercise/exercise1.jpg',
           'assets/images/man/fall/exercise/exercise2.jpg',
           'assets/images/man/fall/exercise/exercise3.jpg',
+          'assets/images/man/fall/exercise/exercise4.jpg',
+          'assets/images/man/fall/exercise/exercise5.jpg',
+          'assets/images/man/fall/exercise/exercise6.jpg',
+          'assets/images/man/fall/exercise/exercise7.jpg',
+          'assets/images/man/fall/exercise/exercise8.jpg',
         ],
       ],
       // 겨울
@@ -136,18 +198,35 @@ class ImagePath {
           'assets/images/man/winter/attendance/attendance3.jpg',
           'assets/images/man/winter/attendance/attendance4.jpg',
           'assets/images/man/winter/attendance/attendance5.jpg',
+          'assets/images/man/winter/attendance/attendance6.jpg',
+          'assets/images/man/winter/attendance/attendance7.jpg',
+          'assets/images/man/winter/attendance/attendance8.jpg',
+          'assets/images/man/winter/attendance/attendance9.jpg',
         ],
         // 캠퍼스
         [
           'assets/images/man/winter/campus/campus.jpg',
           'assets/images/man/winter/campus/campus1.jpg',
           'assets/images/man/winter/campus/campus2.jpg',
+          'assets/images/man/winter/campus/campus3.jpg',
+          'assets/images/man/winter/campus/campus4.jpg',
+          'assets/images/man/winter/campus/campus5.jpg',
+          'assets/images/man/winter/campus/campus6.jpg',
+          'assets/images/man/winter/campus/campus7.jpg',
+          'assets/images/man/winter/campus/campus8.jpg',
+          'assets/images/man/winter/campus/campus9.jpg',
         ],
         // 데이트
         [
           'assets/images/man/winter/date/date.jpg',
           'assets/images/man/winter/date/date1.jpg',
           'assets/images/man/winter/date/date2.jpg',
+          'assets/images/man/winter/date/date3.jpg',
+          'assets/images/man/winter/date/date4.jpg',
+          'assets/images/man/winter/date/date5.jpg',
+          'assets/images/man/winter/date/date6.jpg',
+          'assets/images/man/winter/date/date7.jpg',
+          'assets/images/man/winter/date/date8.jpg',
         ],
         // 운동
         [
@@ -156,6 +235,11 @@ class ImagePath {
           'assets/images/man/winter/exercise/exercise2.jpg',
           'assets/images/man/winter/exercise/exercise3.jpg',
           'assets/images/man/winter/exercise/exercise4.jpg',
+          'assets/images/man/winter/exercise/exercise5.jpg',
+          'assets/images/man/winter/exercise/exercise6.jpg',
+          'assets/images/man/winter/exercise/exercise7.jpg',
+          'assets/images/man/winter/exercise/exercise8.jpg',
+          'assets/images/man/winter/exercise/exercise9.jpg',
         ],
       ],
     ],
@@ -170,6 +254,10 @@ class ImagePath {
           'assets/images/woman/spring/attendance/attendance2.jpg',
           'assets/images/woman/spring/attendance/attendance3.jpg',
           'assets/images/woman/spring/attendance/attendance4.jpg',
+          'assets/images/woman/spring/attendance/attendance5.jpg',
+          'assets/images/woman/spring/attendance/attendance6.jpg',
+          'assets/images/woman/spring/attendance/attendance7.jpg',
+          'assets/images/woman/spring/attendance/attendance8.jpg',
         ],
         // 캠퍼스
         [
@@ -178,6 +266,11 @@ class ImagePath {
           'assets/images/woman/spring/campus/campus2.jpg',
           'assets/images/woman/spring/campus/campus3.jpg',
           'assets/images/woman/spring/campus/campus4.jpg',
+          'assets/images/woman/spring/campus/campus5.jpg',
+          'assets/images/woman/spring/campus/campus6.jpg',
+          'assets/images/woman/spring/campus/campus7.jpg',
+          'assets/images/woman/spring/campus/campus8.jpg',
+          'assets/images/woman/spring/campus/campus9.jpg',
         ],
         // 데이트
         [
@@ -185,6 +278,11 @@ class ImagePath {
           'assets/images/woman/spring/date/date1.jpg',
           'assets/images/woman/spring/date/date2.jpg',
           'assets/images/woman/spring/date/date3.jpg',
+          'assets/images/woman/spring/date/date4.jpg',
+          'assets/images/woman/spring/date/date5.jpg',
+          'assets/images/woman/spring/date/date6.jpg',
+          'assets/images/woman/spring/date/date7.jpg',
+          'assets/images/woman/spring/date/date8.jpg',
         ],
         // 운동
         [
@@ -192,6 +290,11 @@ class ImagePath {
           'assets/images/woman/spring/exercise/exercise1.jpg',
           'assets/images/woman/spring/exercise/exercise2.jpg',
           'assets/images/woman/spring/exercise/exercise3.jpg',
+          'assets/images/woman/spring/exercise/exercise4.jpg',
+          'assets/images/woman/spring/exercise/exercise5.jpg',
+          'assets/images/woman/spring/exercise/exercise6.jpg',
+          'assets/images/woman/spring/exercise/exercise7.jpg',
+          'assets/images/woman/spring/exercise/exercise8.jpg',
         ],
       ],
       // 여름
@@ -202,6 +305,11 @@ class ImagePath {
           'assets/images/woman/summer/attendance/attendance1.jpg',
           'assets/images/woman/summer/attendance/attendance2.jpg',
           'assets/images/woman/summer/attendance/attendance3.jpg',
+          'assets/images/woman/summer/attendance/attendance4.jpg',
+          'assets/images/woman/summer/attendance/attendance5.jpg',
+          'assets/images/woman/summer/attendance/attendance6.jpg',
+          'assets/images/woman/summer/attendance/attendance7.jpg',
+          'assets/images/woman/summer/attendance/attendance8.jpg',
         ],
         // 캠퍼스
         [
@@ -210,6 +318,11 @@ class ImagePath {
           'assets/images/woman/summer/campus/campus2.jpg',
           'assets/images/woman/summer/campus/campus3.jpg',
           'assets/images/woman/summer/campus/campus4.jpg',
+          'assets/images/woman/summer/campus/campus5.jpg',
+          'assets/images/woman/summer/campus/campus6.jpg',
+          'assets/images/woman/summer/campus/campus7.jpg',
+          'assets/images/woman/summer/campus/campus8.jpg',
+          'assets/images/woman/summer/campus/campus9.jpg',
         ],
         // 데이트
         [
@@ -217,6 +330,11 @@ class ImagePath {
           'assets/images/woman/summer/date/date1.jpg',
           'assets/images/woman/summer/date/date2.jpg',
           'assets/images/woman/summer/date/date3.jpg',
+          'assets/images/woman/summer/date/date4.jpg',
+          'assets/images/woman/summer/date/date5.jpg',
+          'assets/images/woman/summer/date/date6.jpg',
+          'assets/images/woman/summer/date/date7.jpg',
+          'assets/images/woman/summer/date/date8.jpg',
         ],
         // 운동
         [
@@ -224,6 +342,11 @@ class ImagePath {
           'assets/images/woman/summer/exercise/exercise1.jpg',
           'assets/images/woman/summer/exercise/exercise2.jpg',
           'assets/images/woman/summer/exercise/exercise3.jpg',
+          'assets/images/woman/summer/exercise/exercise4.jpg',
+          'assets/images/woman/summer/exercise/exercise5.jpg',
+          'assets/images/woman/summer/exercise/exercise6.jpg',
+          'assets/images/woman/summer/exercise/exercise7.jpg',
+          'assets/images/woman/summer/exercise/exercise8.jpg',
         ],
       ],
       // 가을
@@ -235,6 +358,10 @@ class ImagePath {
           'assets/images/woman/fall/attendance/attendance2.jpg',
           'assets/images/woman/fall/attendance/attendance3.jpg',
           'assets/images/woman/fall/attendance/attendance4.jpg',
+          'assets/images/woman/fall/attendance/attendance5.jpg',
+          'assets/images/woman/fall/attendance/attendance6.jpg',
+          'assets/images/woman/fall/attendance/attendance7.jpg',
+          'assets/images/woman/fall/attendance/attendance8.jpg',
         ],
         // 캠퍼스
         [
@@ -242,6 +369,11 @@ class ImagePath {
           'assets/images/woman/fall/campus/campus1.jpg',
           'assets/images/woman/fall/campus/campus2.jpg',
           'assets/images/woman/fall/campus/campus3.jpg',
+          'assets/images/woman/fall/campus/campus4.jpg',
+          'assets/images/woman/fall/campus/campus5.jpg',
+          'assets/images/woman/fall/campus/campus6.jpg',
+          'assets/images/woman/fall/campus/campus7.jpg',
+          'assets/images/woman/fall/campus/campus8.jpg',
         ],
         // 데이트
         [
@@ -249,6 +381,11 @@ class ImagePath {
           'assets/images/woman/fall/date/date1.jpg',
           'assets/images/woman/fall/date/date2.jpg',
           'assets/images/woman/fall/date/date3.jpg',
+          'assets/images/woman/fall/date/date4.jpg',
+          'assets/images/woman/fall/date/date5.jpg',
+          'assets/images/woman/fall/date/date6.jpg',
+          'assets/images/woman/fall/date/date7.jpg',
+          'assets/images/woman/fall/date/date8.jpg',
         ],
         // 운동
         [
@@ -256,6 +393,11 @@ class ImagePath {
           'assets/images/woman/fall/exercise/exercise1.jpg',
           'assets/images/woman/fall/exercise/exercise2.jpg',
           'assets/images/woman/fall/exercise/exercise3.jpg',
+          'assets/images/woman/fall/exercise/exercise4.jpg',
+          'assets/images/woman/fall/exercise/exercise5.jpg',
+          'assets/images/woman/fall/exercise/exercise6.jpg',
+          'assets/images/woman/fall/exercise/exercise7.jpg',
+          'assets/images/woman/fall/exercise/exercise8.jpg',
         ],
       ],
       // 겨울
@@ -266,6 +408,11 @@ class ImagePath {
           'assets/images/woman/winter/attendance/attendance1.jpg',
           'assets/images/woman/winter/attendance/attendance2.jpg',
           'assets/images/woman/winter/attendance/attendance3.jpg',
+          'assets/images/woman/winter/attendance/attendance4.jpg',
+          'assets/images/woman/winter/attendance/attendance5.jpg',
+          'assets/images/woman/winter/attendance/attendance6.jpg',
+          'assets/images/woman/winter/attendance/attendance7.jpg',
+          'assets/images/woman/winter/attendance/attendance8.jpg',
         ],
         // 캠퍼스
         [
@@ -273,6 +420,11 @@ class ImagePath {
           'assets/images/woman/winter/campus/campus1.jpg',
           'assets/images/woman/winter/campus/campus2.jpg',
           'assets/images/woman/winter/campus/campus3.jpg',
+          'assets/images/woman/winter/campus/campus4.jpg',
+          'assets/images/woman/winter/campus/campus5.jpg',
+          'assets/images/woman/winter/campus/campus6.jpg',
+          'assets/images/woman/winter/campus/campus7.jpg',
+          'assets/images/woman/winter/campus/campus8.jpg',
         ],
         // 데이트
         [
@@ -280,6 +432,11 @@ class ImagePath {
           'assets/images/woman/winter/date/date1.jpg',
           'assets/images/woman/winter/date/date2.jpg',
           'assets/images/woman/winter/date/date3.jpg',
+          'assets/images/woman/winter/date/date4.jpg',
+          'assets/images/woman/winter/date/date5.jpg',
+          'assets/images/woman/winter/date/date6.jpg',
+          'assets/images/woman/winter/date/date7.jpg',
+          'assets/images/woman/winter/date/date8.jpg',
         ],
         // 운동
         [
@@ -287,6 +444,11 @@ class ImagePath {
           'assets/images/woman/winter/exercise/exercise1.jpg',
           'assets/images/woman/winter/exercise/exercise2.jpg',
           'assets/images/woman/winter/exercise/exercise3.jpg',
+          'assets/images/woman/winter/exercise/exercise4.jpg',
+          'assets/images/woman/winter/exercise/exercise5.jpg',
+          'assets/images/woman/winter/exercise/exercise6.jpg',
+          'assets/images/woman/winter/exercise/exercise7.jpg',
+          'assets/images/woman/winter/exercise/exercise8.jpg',
         ],
       ],
     ],
